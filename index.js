ff34104b3ecc9eecd58d77d1d5777f448deb1fa5
Jose Enrique Guardado Peralta <genjumper@gmail.com>
2021-03-18 20:06:28 -0600

    Agregado el puerto indicado por heroku

--- index.js
+++ index.js
@@ -32,8 +32,11 @@ app.get('/movie/:title', (req, res) =>{
 
 
 
-
-
-app.listen(port, () =>{
-    console.log(`Example app listening at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
+
+//app.listen(port, () =>{
+ //   console.log(`Example app listening at http://localhost:${port}`);
+//});
